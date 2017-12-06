7edd6f0361f7433aba1fde1f2c8928bbd03d8be1
lee <jcsoft@aliyun.com>
2017-12-06 09:48:47 +0800

    add WEBROOT DOMAIN

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
+# Set custom webroot
+if [ ! -z "$WEBROOT" ]; then
+ sed -i "s#"/var/www/html"#"${WEBROOT}"#g" /etc/apache2/httpd.conf
+ sed -i "s#/var/www/html/$1#${WEBROOT}/$1#g" /etc/apache2/httpd.conf
+else
+ webroot=/var/www/html
+fi
+
+if [ ! -z "$DOMAIN" ]; then
+ sed -i "s#ServerName localhost:80#ServerName $DOMAIN:80#g" /etc/apache2/httpd.conf
+fi
+
 exec /usr/local/bin/httpd-foreground
